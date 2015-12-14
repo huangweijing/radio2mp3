1e8d70a1ae528e54e015d6a72856604294382b9e
huangweijing <huangweijinglove@gmail.com>
2015-12-15 00:33:35 +0900

    file overwrite

--- labcode/test1/downloadMP3.bat
+++ labcode/test1/downloadMP3.bat
@@ -1,2 +1,2 @@
 ..\..\tools\rtmpdump-2.4\rtmpdump -r "rtmpe://fms1.uniqueradio.jp/" -y "aandg11" -a "?rtmp://fms-base1.mitene.ad.jp/agqr/" -A 0 -B 600 -v -o output.flv
-..\..\tools\ffmpeg\bin\ffmpeg -i output.flv output.mp3
+..\..\tools\ffmpeg\bin\ffmpeg -y -i output.flv output.mp3
